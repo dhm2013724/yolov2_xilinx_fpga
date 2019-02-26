337e878122f50c9649da4142106aa5e4546fa64b
dhm2013724 <656314839@qq.com>
2019-02-26 10:45:19 +0800

    Update script.tcl

--- hls/script.tcl
+++ hls/script.tcl
@@ -8,6 +8,6 @@ set_top YOLO2_FPGA
 add_files cnn.cpp
 open_solution -reset "solution1"
 set_part {xc7z020clg400-1}
-create_clock -period 5.4 -name default
+create_clock -period 5.2 -name default
 csynth_design
 export_design -rtl verilog -format ip_catalog
